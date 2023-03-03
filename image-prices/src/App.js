6ffb232cb99e1e8ac94e6105f5405bc226f25b8a
johncorbett2 <corbett.john.j@gmail.com>
2023-03-02 20:13:57 -0500

    created text box components

--- image-prices/src/App.js
+++ image-prices/src/App.js
@@ -1,10 +1,14 @@
-
+import React from "react";
+import Dropdown from "./Dropdown";
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header>Hello world</header>
+      <header>Step 1: What image are you searching for today?</header>
+      <br></br>
+      <Dropdown />
     </div>
   );
 }
---
+++ image-prices/src/Dropdown.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Dropdown() {
+    return <div>
+        <form>
+            <label>Choose Image</label>
+            <input placeholder="Enter Image"/>
+            <label>Choose Body Part</label>
+            <input placeholder="Enter Body Part"/>
+            <label>Choose Contrast</label>
+            <input placeholder="Enter Contrast"/>
+        </form>
+    </div>
+
+}
+
+export default Dropdown;
